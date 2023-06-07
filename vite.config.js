3afbfe4c84b41213ff06582ea8cf38f8e060766a
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-06-07 14:22:13 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://keven-bardales.github.io/twitterFollowCard_REACT',
+  base: '/twitterFollowCard_REACT',
 });
